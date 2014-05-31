59d0242e850dedeb68292f1491b0abb5c037acfa
Dominik Zogg <dominik.zogg@gmail.com>
2014-05-31 20:34:12 +0200

    no need for annotation loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,3 @@
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Dflydev\Tests', __DIR__);
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
